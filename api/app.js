6f3856f5fe0a054320cab385b7d588ba35de2bc5
nthanhtam <tam.nguyen1@tekmedi.vn>
2024-05-28 01:20:08 +0700

    push code

--- api/app.js
+++ api/app.js
@@ -1,10 +1,14 @@
 // Nếu k có module trong package thì phải add lib bằng require
 import express from 'express';
 import postRoute from './routes/post.auth.js';
+import authRoute from './routes/auth.route.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/posts', postRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
 	console.log('Server is running');
